b5d38b864d98837b94c222703196bebb95fac5d5
Noah <noele-flohr@t-online.de>
2024-06-01 13:32:50 +0200

    added Print and Reading in Sudokus

--- Sudoku.py
+++ Sudoku.py
@@ -1,24 +1,85 @@
 import numpy as np
-
+import sys
 class Sudoku:
     def __init__(self):
-        entries = np.zeros(9,9)
+        self.entries = np.zeros((9,9), dtype = int)
         self.Create_Sudoku_Manually()
     
+    def __init__(self, array):
+        self.entries = array
+        
+    def get_entries(self) -> np.ndarray:
+        return self.entries
+    
     def Create_Sudoku_Manually(self):
-        for col, row in self.entries:
-            valid_input = False
-            while not valid_input:
-                try:
-                    value = int(input(f"Enter value for cell ({row+1}, {col+1}) [0 for empty]: "))
-                    if 0 <= value <= 9:
-                        self.entries[row, col] = value
-                        valid_input = True
-                    else:
-                        print("Please enter a number between 0 and 9.")
-                except ValueError:
-                    print("Invalid input. Please enter an integer between 0 and 9.")
+        for row in range (self.entries.shape[0]):
+            for col in range (self.entries.shape[1]):                    
+                valid_input = False
+                while not valid_input:
+                    try:
+                        value = int(input(f"Enter value for cell ({row+1}, {col+1}) [0 for empty]: "))
+                        if 0 <= value <= 9:
+                            self.entries[row, col] = value
+                            valid_input = True
+                        else:
+                            print("Please enter a number between 0 and 9.")
+                    except ValueError:
+                        print("Invalid input. Please enter an integer between 0 and 9.")
 
+    def Create_Sudoku_Read(self, array):
+        print()
+        
+        
+class Sudoku_Helper:
+    def Get_Row_Col(self, sudoku, row, col) -> np.array:
+        entries = sudoku.get_entries()
+        neighbours = np.zeros((9), dtype= bool)
+        #check column
+        for i in range(9):
+            if(i != col):
+                current = sudoku.entries[row][i]
+                if(current != 0):
+                    neighbours[current -1] = True
+        for i in range(9):
+            if(i != row):
+                current = sudoku.entries[i][col]
+                if(current != 0):
+                    neighbours[current -1] = True
+        return neighbours
+                
+    #def Get_Block(self, sudoku, row, col) -> np.array:
+    
+
+    
+    
+    def Print(sudoku):
+        entries = sudoku.get_entries()
+        frame_length = 3*9 + 2 + 6
+        
+        print("Sudoku: ")
+        for row in range (entries.shape[0]):
+            if(row == 0 or row == 3 or row == 6):
+                for i in range(3*9 + 4 + 6):
+                    sys.stdout.write("-")
+                print()
+            for col in range (entries.shape[1]):  
+                if(col == (0)):
+                    sys.stdout.write("| ")
+                if(entries[row][col] == 0):
+                    sys.stdout.write("   ")
+                else:
+                    sys.stdout.write(" " + str(entries[row][col]) + " ")
+                if(col == 2 or col == 5): 
+                    sys.stdout.write(" | ")
+                elif(col == (8)):
+                    sys.stdout.write(" |")
+                    
+            print()
+        for i in range(frame_length +2 ):
+            sys.stdout.write("-")
+        print()
+            
+            
 class SolvingSudoku:
     def __init__():
         sudoku = Sudoku()
--- Test_Sudoku_Creation.py
+++ Test_Sudoku_Creation.py
@@ -1,3 +1,18 @@
-import Sudoku as Sudoku
+from Sudoku import Sudoku
+from Sudoku import Sudoku_Helper
+import numpy as np
 
-test_sudoku = Sudoku()
+sudoku = np.array([[0,7,0,5,8,3,0,2,0],
+          [0,5,9,2,0,0,3,0,0],
+          [3,4,0,0,0,6,5,0,7],
+          [7,9,5,0,0,0,6,3,2],
+          [0,0,3,6,9,7,1,0,0],
+          [6,8,0,0,0,2,7,0,0],
+          [9,1,4,8,3,5,0,7,6],
+          [0,3,0,7,0,1,4,9,5],
+          [5,6,7,4,2,9,0,1,3]
+          ])
+
+test_sudoku = Sudoku(sudoku)
+
+Sudoku_Helper.Print(test_sudoku)
