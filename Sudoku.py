68b9cc4b184d403f529930878610b7fddab87ca7
Noah <noele-flohr@t-online.de>
2024-06-03 15:15:36 +0200

    added updating by rowcol and block

--- Sudoku.py
+++ Sudoku.py
@@ -1,26 +1,28 @@
 import numpy as np
 import sys
-import sys
-class Sudoku:
-    def __init__(self):
-        self.self.entries = np.zeros((9,9), dtype = int)
-        # Erstelle ein Array mit den Zahlen 1 bis 9
-        numbers = np.arange(1, 10)
-        # Wiederhole dieses Array für jedes Feld im 9x9-Grid
-        self.pos_entries = np.tile(numbers, (9, 9, 1))
 
-        self.Create_Sudoku_Manually()
-    
-    def __init__(self, array):
-        self.entries = array
+class Sudoku:
+    def __init__(self, array=None):
+        if array is None:
+            self.entries = np.zeros((9, 9), dtype=int)
+            self.Create_Sudoku_Manually()
+        else:
+            self.entries = array
+        # Erstelle ein Array mit den Zahlen 1 bis 9
+        true_array = np.ones(9, dtype=bool)
+        # Wiederhole dieses Array für jedes Feld im 9x9-Grid
+        self.pos_entries = np.tile(true_array, (9, 9, 1))
         
-    def get_entries(self) -> np.ndarray:
+    def get_Entries(self) -> np.ndarray:
         return self.entries
+
+    def get_Pos_Entries(self) -> np.ndarray:
+        return self.pos_entries
     
-    def __init__(self, array):
-        self.entries = array
+    def set_Pos_Entries(self, pos_entries):
+        self.pos_entries = pos_entries
         
-    def get_entries(self) -> np.ndarray:
+    def get_Entries(self) -> np.ndarray:
         return self.entries
     
     def Create_Sudoku_Manually(self):
@@ -43,29 +45,9 @@ class Sudoku:
         
         
 class Sudoku_Helper:
-    def Get_Row_Col(self, sudoku, row, col) -> np.array:
-        entries = sudoku.get_entries()
-        neighbours = np.zeros((9), dtype= bool)
-        #check column
-        for i in range(9):
-            if(i != col):
-                current = sudoku.entries[row][i]
-                if(current != 0):
-                    neighbours[current -1] = True
-        for i in range(9):
-            if(i != row):
-                current = sudoku.entries[i][col]
-                if(current != 0):
-                    neighbours[current -1] = True
-        return neighbours
-                
-    #def Get_Block(self, sudoku, row, col) -> np.array:
-    
 
-    
-    
     def Print(sudoku):
-        entries = sudoku.get_entries()
+        entries = sudoku.get_Entries()
         frame_length = 3*9 + 2 + 6
         
         print("Sudoku: ")
@@ -90,29 +72,13 @@ class Sudoku_Helper:
         for i in range(frame_length +2 ):
             sys.stdout.write("-")
         print()
+    
+    def Print_Pos_Entries(sudoku, row, col):
+        print(sudoku.get_Pos_Entries()[row][col])    
             
-            
-        for row in range (entries.shape[0]):
-            for col in range (entries.shape[1]):                    
-                valid_input = False
-                while not valid_input:
-                    try:
-                        value = int(input(f"Enter value for cell ({row+1}, {col+1}) [0 for empty]: "))
-                        if 0 <= value <= 9:
-                            entries[row, col] = value
-                            valid_input = True
-                        else:
-                            print("Please enter a number between 0 and 9.")
-                    except ValueError:
-                        print("Invalid input. Please enter an integer between 0 and 9.")
-
-    def Create_Sudoku_Read(self, array):
-        print()
-        
-        
-class Sudoku_Helper:
-    def Get_Row_Col(self, sudoku, row, col) -> np.ndarray:
-        entries = sudoku.get_entries()
+class Sudoku_Solver:
+    def Get_Row_Col(sudoku, row, col) -> np.ndarray:
+        entries = sudoku.get_Entries()
         neighbours = np.zeros((9), dtype= bool)
         #check column
         for i in range(9):
@@ -125,10 +91,11 @@ class Sudoku_Helper:
                 current = entries[i][col]
                 if(current != 0):
                     neighbours[current -1] = True
+        #print(neighbours)
         return neighbours
                 
-    def Get_Block(self, sudoku, row, col) -> np.ndarray:
-        entries = sudoku.get_entries()
+    def Get_Block(sudoku, row, col) -> np.ndarray:
+        entries = sudoku.get_Entries()
         neighbours = np.zeros((9), dtype= bool)
         block_row = row // 3 
         block_col = col // 3
@@ -138,41 +105,23 @@ class Sudoku_Helper:
                 current = entries[i][j]
                 if(current != 0):
                     neighbours[current -1] = True
-
+        #print(neighbours)
         return neighbours
-            
-
     
+    def Update_Possible_Entries(sudoku, neighbours, col, row):
+        pos_entries = sudoku.get_Pos_Entries()
+        for i in range(0,9):
+            if(neighbours[i]):
+                pos_entries[row][col][i] = False
+        print("row: ", row,  "col: ", col,  "pos_entries: ", pos_entries[row][col])
+        sudoku.set_Pos_Entries(pos_entries)
     
-    def Print(sudoku):
-        entries = sudoku.get_entries()
-        frame_length = 3*9 + 2 + 6
-        
-        print("Sudoku: ")
-        for row in range (entries.shape[0]):
-            if(row == 0 or row == 3 or row == 6):
-                for i in range(3*9 + 4 + 6):
-                    sys.stdout.write("-")
-                print()
-            for col in range (entries.shape[1]):  
-                if(col == (0)):
-                    sys.stdout.write("| ")
-                if(entries[row][col] == 0):
-                    sys.stdout.write("  ")
-                else:
-                    sys.stdout.write(" " + str(entries[row][col]) + " ")
-                if(col == 2 or col == 5): 
-                    sys.stdout.write(" | ")
-                elif(col == (8)):
-                    sys.stdout.write(" |")
-                    
-            print()
-        for i in range(frame_length +2 ):
-            sys.stdout.write("-")
-        print()
-            
-            
-class SolvingSudoku:
-    def __init__():
-        sudoku = Sudoku()
+    #checks for all entries the box and row and line neighbours
+    def Update_Box_RowCol(sudoku):
+        for i in range(0,9):
+            for j in range(0,9):
+                neighbours = Sudoku_Solver.Get_Block(sudoku, i,j) | Sudoku_Solver.Get_Row_Col(sudoku, i, j)
+                Sudoku_Solver.Update_Possible_Entries(sudoku, neighbours,i,j)
+                print("row: ", i,  "col: ", j,  "neighbours: ", neighbours)
+                
 
--- Test_Sudoku_Creation.py
+++ Test_Sudoku_Creation.py
@@ -1,5 +1,6 @@
 from Sudoku import Sudoku
 from Sudoku import Sudoku_Helper
+from Sudoku import Sudoku_Solver
 import numpy as np
 
 sudoku = np.array([[0,7,0,5,8,3,0,2,0],
@@ -16,3 +17,12 @@ sudoku = np.array([[0,7,0,5,8,3,0,2,0],
 test_sudoku = Sudoku(sudoku)
 
 Sudoku_Helper.Print(test_sudoku)
+#entries =test_sudoku.get_Pos_Entries()
+block_neigbours = Sudoku_Solver.Get_Block(test_sudoku, 0, 0)
+rowcol_neighbours = Sudoku_Solver.Get_Row_Col(test_sudoku, 0, 0)
+common_neighbours = block_neigbours | rowcol_neighbours
+print(common_neighbours)
+#Sudoku_Solver.Update_Possible_Entries(test_sudoku, block_neigbours, 0, 0)
+#Sudoku_Solver.Update_Possible_Entries(test_sudoku, rowcol_neighbours, 0, 0)
+#Sudoku_Solver.Print_Pos_Entries(test_sudoku, 0, 0)
+Sudoku_Solver.Update_Box_RowCol(test_sudoku)
